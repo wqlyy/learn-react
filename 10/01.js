2d0b29aa1f994a1f45e5371e9e17929e7634fedd
wangqiang <wangqiangweb@163.com>
2017-09-01 10:30:28 +0800

    数据双向绑定

--- 10/01.js
+++ 10/01.js
@@ -1,3 +1,28 @@
 /**
  * Created by wangqiang on 2017/8/31.
  */
+// 数据双向绑定
+const LinkedStateMixin = React.addons.LinkedStateMixin;
+
+const Comp = React.createClass({
+    displayName:'Comp',
+    mixins:[LinkedStateMixin],
+    getInitialState(){
+        return {
+            name:''
+        }
+    },
+   changeName(e){
+        this.setState({
+            name:e.target.value
+        })
+    },
+    render(){
+        return <div>
+            {this.state.name}
+            <input type="text" valueLink={this.linkState('name')} />
+        </div>
+    }
+});
+
+ReactDOM.render(<Comp/>,document.body)
